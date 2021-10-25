1f587541c5802d43706ef12eddf99e60214f725f
Luneau <paul.luneau@mail.com>
2021-10-25 16:03:49 +0200

    Création de la solution

---
+++ Miniville/Miniville/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Miniville
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
